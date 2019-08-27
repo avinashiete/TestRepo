5e7bfde5760498737eb1f4e220da4bf55681f47d
avinash <avinash@Acer>
2019-08-27 14:36:39 +0530

    hjh

---
+++ demo/src/main/java/io/agileintelligence/ppmtool/Project.java
@@ -0,0 +1,14 @@
+package io.agileintelligence.ppmtool;
+
+
+
+import javax.persistence.Entity;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Project {
+
+@Id	
+Integer id;
+}
